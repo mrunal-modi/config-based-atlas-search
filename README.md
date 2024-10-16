1ee83eef61e789f8ac6ce37e2f9ba6323b5fc894
mrunal-modi <mrunal.modi@gmail.com>
2024-10-16 13:51:01 +1100

    GitHub header

--- README.md
+++ README.md
@@ -7,9 +7,6 @@ This project allowing users to deploy MongoDB Atlas Search by updating a simple 
 - [Auth0](https://manage.auth0.com/)
 - [MongoDB Atlas Search](https://cloud.mongodb.com/)
 
+## Demo
 
-## Learn More
-
-For step by step declaratice documentation follow MyExampleApp.com
-
-- [Config based MongoDB Atlas Search Next.js Example](https://myexampleapp.com)
+- https://search.mongodbmethods.com/
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -67,8 +67,8 @@ const Header: React.FC = () => {
           <NextLink href="https://mongodbmethods.com/" passHref legacyBehavior>
             <StyledLink variant="body2" component="a">Home</StyledLink>
           </NextLink>
-          <NextLink href="https://www.myexampleapp.com/documents/view/66fd303ea14b0e7c350ee3e6" passHref legacyBehavior>
-            <StyledLink variant="body2" component="a">Documentation</StyledLink>
+          <NextLink href="https://github.com/mrunal-modi/config-based-mongodb-atlas-search-next" passHref legacyBehavior>
+            <StyledLink variant="body2" component="a">GitHub</StyledLink>
           </NextLink>
         </Box>
         <UserSection>
